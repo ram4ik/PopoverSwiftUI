b268825c214821a08d31d6c5c166a3484555626c
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-01 21:40:22 +0300

    Initial Commit

---
+++ PopoverSwiftUI/PopoverSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopoverSwiftUIApp.swift
+//  PopoverSwiftUI
+//
+//  Created by Ramill Ibragimov on 01.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct PopoverSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
